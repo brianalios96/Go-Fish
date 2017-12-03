2c4ae11cce284da3a32470a8a06df3d0bad086ce
Isaac Plunkett <isaacp@email.arizona.edu>
2017-12-03 10:00:11 -0700

    removed GUI

--- src/Card.java
+++ src/Card.java
@@ -1,27 +1,18 @@
 import java.io.Serializable;
 
-import javafx.scene.image.Image;
-
 public class Card implements Comparable<Card>, Serializable
 {
 	private static final long serialVersionUID = 3563714765503218503L;
 	private CardRank rank;
-	private Image picture;
 	
-	public Card(CardRank rank, Image picture)
+	public Card(CardRank rank)
 	{
 		this.rank = rank;
-		this.picture = picture;
 	}
 	
 	public CardRank getRank()
 	{
 		return rank;
-	}
-	
-	public Image getPicture()
-	{
-		return picture;
 	}
 
 	@Override
